8c3b672db746457435cce1792e33a71b757e535b
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2022-12-09 15:19:14 +0530

    NewData

--- git practice assignments/index.js
+++ git practice assignments/index.js
@@ -1,26 +1,26 @@
 
 function checkprime(num){
     let count=0
-    for(let i=0;i<=num;i++){
+    for(let i=1;i<=num;i++){
         if(num%i==0){
             count++;
         }
     }
     if(count==1)
     {
-        return false;
+        return true;
     }
    else{
-        return true;
+        return false;
      }
     
 }
 
 for(let i=1;i<=13;i++){
-    let output=checkprime(i)
-    if(output==true){
-        console.log(i,'is a Prime Number')
+    let output = checkprime(i)
+    if(output===true){
+        console.log(i,' is a Prime Number ')
     }else{
-        console.log(i,'its not a Prime Number')
+        console.log(i,' its not a Prime Number ')
     }
 }
